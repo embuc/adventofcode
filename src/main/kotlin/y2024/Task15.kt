c575ebd82d370c1c0d14820f4a08066b2ad409aa
Emir Bucalovic <emir.bucalovic@tactel.se>
2024-12-15 19:12:47 +0100

    Task 15 part II.

--- src/main/kotlin/y2024/Task15.kt
+++ src/main/kotlin/y2024/Task15.kt
@@ -1,6 +1,7 @@
 package y2024
 
 import Task
+import sun.security.util.PropertyExpander.expand
 
 /*--- Day 15: Warehouse Woes ---*/
 class Task15(val input: List<String>) : Task {
@@ -10,46 +11,43 @@ class Task15(val input: List<String>) : Task {
 		val instructions = input
 			.dropWhile { it.isNotEmpty() }
 			.drop(1)
-			.joinToString("") 
+			.joinToString("")
 			.split("").filter { it.isNotEmpty() }
 		var player = grid.mapIndexed { i, row -> Pair(i, row.indexOf('@')) }.filter { it.second != -1 }.first()
 		for (direction in instructions) {
-//			println("\nBefore instruction: $i \n")
-//			printGrid(grid)
 			if (!wall(grid, player, direction)) {
 				val emptySpace = findEmptySpaceToMoveIn(grid, player, direction)
 				if (emptySpace.first != -1) {
 					player = movePlayerAndBoxes(grid, player, direction, emptySpace)
 				}
 			}
-//			println("\nAfter instruction: $i \n")
-//			printGrid(grid)
 		}
-		return calculateSumOfGpsCoordinates(grid)
+		return calculateSumOfGpsCoordinates(grid, 'O')
 	}
 
-	private fun calculateSumOfGpsCoordinates(grid: List<CharArray>): Long {
+	private fun calculateSumOfGpsCoordinates(grid: List<CharArray>, char:Char): Long {
 		var sum = 0L
 		grid.forEachIndexed { i, row ->
 			row.forEachIndexed { j, c ->
-				if (c == 'O') {
-					sum += 100L*i + j
+				if (c == char) {
+					sum += 100L * i + j
 				}
 			}
 		}
 		return sum
 	}
 
-	private fun movePlayerAndBoxes(grid: List<CharArray>, player: Pair<Int, Int>, s: String, emptySpace:Pair<Int, Int>): Pair<Int, Int> {
+	private fun movePlayerAndBoxes(grid: List<CharArray>,player: Pair<Int, Int>,s: String,emptySpace: Pair<Int, Int>): Pair<Int, Int> {
 		grid[player.first][player.second] = '.'
 		when (s) {
 			"v" -> {
-				for (i in player.first + 2 .. emptySpace.first) {
+				for (i in player.first + 2..emptySpace.first) {
 					grid[i][player.second] = 'O'
 				}
 				grid[player.first + 1][player.second] = '@'
 				return player.copy(first = player.first + 1)
 			}
+
 			"^" -> {
 				for (i in player.first - 2 downTo emptySpace.first) {
 					grid[i][player.second] = 'O'
@@ -57,13 +55,15 @@ class Task15(val input: List<String>) : Task {
 				grid[player.first - 1][player.second] = '@'
 				return player.copy(first = player.first - 1)
 			}
+
 			">" -> {
-				for (i in player.second + 2 .. emptySpace.second) {
+				for (i in player.second + 2..emptySpace.second) {
 					grid[player.first][i] = 'O'
 				}
 				grid[player.first][player.second + 1] = '@'
 				return player.copy(second = player.second + 1)
 			}
+
 			"<" -> {
 				for (i in player.second - 2 downTo emptySpace.second) {
 					grid[player.first][i] = 'O'
@@ -71,6 +71,7 @@ class Task15(val input: List<String>) : Task {
 				grid[player.first][player.second - 1] = '@'
 				return player.copy(second = player.second - 1)
 			}
+
 			else -> return player
 		}
 	}
@@ -114,6 +115,211 @@ class Task15(val input: List<String>) : Task {
 	}
 
 	override fun b(): Any {
-		return 0
+		val grid = expand(input.subList(0, input.indexOf("")).map { it.toCharArray() })
+//		printGrid(grid)
+		val instructions = input
+			.dropWhile { it.isNotEmpty() }
+			.drop(1)
+			.joinToString("")
+			.split("").filter { it.isNotEmpty() }
+		var player = grid.mapIndexed { i, row -> Pair(i, row.indexOf('@')) }.filter { it.second != -1 }.first()
+		for (direction in instructions) {
+//			println("\nBefore instruction: $direction \n")
+//			printGrid(grid)
+			if (!wall(grid, player, direction)) {
+				when (direction) {
+					"v" -> {
+						if (canMoveBoxesUPDown(grid, player.first, player.second, direction)) {
+							player = movePlayerAndBoxesUPDown(grid,  player.first, player.second, direction)
+						}
+					}
+					"^" -> {
+						if (canMoveBoxesUPDown(grid, player.first, player.second, direction)) {
+							player = movePlayerAndBoxesUPDown(grid,  player.first, player.second, direction)
+						}
+					}
+					">" -> {
+						if (canMoveBoxesLeftRight(grid, player, direction)) {
+							player = movePlayerAndBoxesLeftRight(grid, player, direction)
+						}
+					}
+					"<" -> {
+						if (canMoveBoxesLeftRight(grid, player, direction)) {
+							player = movePlayerAndBoxesLeftRight(grid, player, direction)
+						}
+					}
+				}
+			}
+//			println("\nAfter instruction: $direction \n")
+//			printGrid(grid)
+//			println()
+		}
+		return calculateSumOfGpsCoordinates(grid, '[')
+	}
+
+	private fun canMoveBoxesLeftRight(grid: List<CharArray>, player: Pair<Int, Int>, direction: String): Boolean {
+		val (x, y) = player
+		val (dx, dy) = when (direction) {
+			">" -> y + 1 until grid[0].size to {j:Int -> Pair(x, j)} // Right
+
+			"<" -> y -1 downTo 0 to {j: Int -> Pair(x, j)} // Left
+			else -> return false
+		}
+
+		for (i in dx) {
+			val (newX, newY) = dy(i)
+			when (grid[newX][newY]) {
+				'.' -> return true
+				'#' -> return false
+			}
+		}
+		return false
+	}
+
+	private fun movePlayerAndBoxesLeftRight(grid: List<CharArray>,player: Pair<Int, Int>,direction: String): Pair<Int, Int> {
+		val (x, y) = player
+		val (dx, dy) = when (direction) {
+			">" -> y + 1 until grid[0].size to {j:Int -> Pair(x, j)} // Right
+			"<" -> y -1 downTo 0 to {j: Int -> Pair(x, j)} // Left
+			else -> return player
+		}
+
+		var emptySpace = Pair(-1, -1)
+
+		for (i in dx) {
+			val (newX, newY) = dy(i)
+			when (grid[newX][newY]) {
+				'.' -> {
+					emptySpace = Pair(newX, newY)
+					break
+				}
+			}
+		}
+		// fill all tiles with double boxes between the player and the empty space (i.e. player minus one space )
+		when(direction) {
+			">" -> {
+				for (i in y + 2 .. emptySpace.second step 2) {
+					grid[x][i] = '['
+					grid[x][i +1] = ']'
+				}
+				grid[x][y + 1] = '@'
+				grid[x][y] = '.'
+				return player.copy(second = y + 1)
+			}
+			"<" -> {
+				for (i in y - 2 downTo emptySpace.second step 2) {
+					grid[x][i] = ']'
+					grid[x][i-1] = '['
+				}
+				grid[x][y - 1] = '@'
+				grid[x][y] = '.'
+				return player.copy(second = y - 1)
+			}
+			else -> return player
+		}
+	}
+
+	private fun canMoveBoxesUPDown(grid: List<CharArray>,x: Int,y: Int,direction: String): Boolean {
+
+		// Get the next position based on direction
+		val (dx, dy) = when (direction) {
+			"v" -> Pair(1, 0) // Down
+			"^" -> Pair(-1, 0) // Up
+			else -> return false
+		}
+
+		val nextX = x + dx
+		val nextY = y + dy
+
+		// Base cases: if next is wall, return false, or if another box is in the way, fire of 2xrecursion
+
+		when (grid[nextX][nextY]) {
+			'.' -> return true // Found an empty space, move is possible
+			'#' -> return false // Hit a wall
+			'[' -> {
+				val left = canMoveBoxesUPDown(grid, nextX, nextY, direction )
+				val right = canMoveBoxesUPDown(grid, nextX, nextY+1, direction)
+				return left && right
+			}
+			']' -> {
+				val left = canMoveBoxesUPDown(grid, nextX, nextY, direction)
+				val right = canMoveBoxesUPDown(grid, nextX, nextY-1, direction)
+				return left && right
+			}
+		}
+
+		return true
+	}
+
+	private fun movePlayerAndBoxesUPDown(grid: List<CharArray>,x: Int, y: Int,direction: String) : Pair<Int, Int> {
+		val (dx, dy) = when (direction) {
+			"v" -> Pair(1, 0) // Down
+			"^" -> Pair(-1, 0) // Up
+			else -> Pair(0, 0)
+		}
+
+		val nextX = x + dx
+		val nextY = y + dy
+
+		// Base cases: if next is wall, return false, or if another box is in the way, fire of 2xrecursion
+		when (grid[nextX][nextY]) {
+			'.' -> {
+					// Found an empty space, move is possible
+					grid[nextX][nextY] = grid[x][y]
+					grid[x][y] = '.'
+					return Pair(nextX, nextY)
+				}
+			'[' -> {
+				movePlayerAndBoxesUPDown(grid, nextX, nextY, direction)
+				movePlayerAndBoxesUPDown(grid, nextX, nextY+1, direction)
+				grid[nextX][nextY] = grid[x][y]
+				grid[x][y] = '.'
+			}
+			']' -> {
+				movePlayerAndBoxesUPDown(grid, nextX, nextY, direction)
+				movePlayerAndBoxesUPDown(grid, nextX, nextY-1, direction)
+				grid[nextX][nextY] = grid[x][y]
+				grid[x][y] = '.'
+			}
+		}
+
+		return Pair(nextX, nextY)
+	}
+
+	private fun expand(map: List<CharArray>): List<CharArray> {
+		val expanded = mutableListOf<CharArray>()
+//		these are the rules:
+//		If the tile is #, the new map contains ## instead.
+//		If the tile is O, the new map contains [] instead.
+//		If the tile is ., the new map contains .. instead.
+//		If the tile is @, the new map contains @. instead.
+		map.forEach { row ->
+			val newRow = CharArray(row.size * 2)
+			row.forEachIndexed { i, c ->
+				when (c) {
+					'#' -> {
+						newRow[2 * i] = '#'
+						newRow[2 * i + 1] = '#'
+					}
+
+					'O' -> {
+						newRow[2 * i] = '['
+						newRow[2 * i + 1] = ']'
+					}
+
+					'.' -> {
+						newRow[2 * i] = '.'
+						newRow[2 * i + 1] = '.'
+					}
+
+					'@' -> {
+						newRow[2 * i] = '@'
+						newRow[2 * i + 1] = '.'
+					}
+				}
+			}
+			expanded.add(newRow)
+		}
+		return expanded
 	}
 }
--- src/test/kotlin/y2024/Task15Test.kt
+++ src/test/kotlin/y2024/Task15Test.kt
@@ -29,6 +29,7 @@ class Task15Test {
 		^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 		v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 	""".trimIndent().lines()
+
 	val small_input = """
 		########
 		#..O.O.#
@@ -42,6 +43,44 @@ class Task15Test {
 		<^^>>>vv<v>>v<<
 	""".trimIndent().lines()
 
+	val small_input_b1 = """
+		########
+		########
+		########
+		##@.O..#
+		########
+		########
+		########
+		########
+		
+		<>>>>>>>>>>
+	""".trimIndent().lines()
+
+	val small_input_b2 = """
+		########
+		####.###
+		####.###
+		####O###
+		####.###
+		####@###
+		########
+		########
+		
+		v^^^^^^^^^^^^
+	""".trimIndent().lines()
+
+	val small_input_b3 = """
+		#######
+		#...#.#
+		#.....#
+		#..OO@#
+		#..O..#
+		#.....#
+		#######
+		
+		<vv<<^^<<^^
+	""".trimIndent().lines()
+
 	@Test
 	fun a() {
 		assertEquals(2028L, Task15(small_input).a())
@@ -52,5 +91,12 @@ class Task15Test {
 
 	@Test
 	fun b() {
+		assertEquals(1751L, Task15(small_input).b())
+		assertEquals(312L, Task15(small_input_b1).b())
+		assertEquals(108L, Task15(small_input_b2).b())
+		assertEquals(618L, Task15(small_input_b3).b())
+		assertEquals(9021L, Task15(test_input).b())
+		val input = utils.readInputAsListOfStrings("~/git/aoc-inputs/2024/2024_15.txt")
+		assertEquals(1381446L, Task15(input).b())
 	}
 }
