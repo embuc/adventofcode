31b29c4ac2c1af69d493dd3ab15f8fe62d8076d5
Emir Bucalovic <emir.bucalovic@tactel.se>
2024-12-17 19:04:53 +0100

    Updated test.

--- src/test/kotlin/y2024/Task17Test.kt
+++ src/test/kotlin/y2024/Task17Test.kt
@@ -22,12 +22,20 @@ class Task17Test {
 		Program: 2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0
 	""".trimIndent().lines()
 
+	val test_inputb = """
+		Register A: 2024
+		Register B: 0
+		Register C: 0
+		
+		Program: 0,3,5,4,3,0
+	""".trimIndent().lines()
+
 	@Test
 	fun a() {
 		assertEquals("4,6,3,5,6,3,5,2,1,0", Task17(test_input1).a())
 		assertEquals("7,0,3,1,2,6,3,7,1", Task17(test_input2).a())
 		val input = readInputAsListOfStrings("~/git/aoc-inputs/2024/2024_17.txt")
-		assertEquals(0, Task17(input).a())
+		assertEquals("2,0,7,3,0,3,1,3,7", Task17(input).a())
 	}
 
 	@Test
