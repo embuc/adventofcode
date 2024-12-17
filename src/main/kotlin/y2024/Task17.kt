5bc6968760132c54f1844ee99de007376724985d
Emir Bucalovic <emir.bucalovic@tactel.se>
2024-12-17 19:03:08 +0100

    Task 17 part I.

--- src/main/kotlin/y2024/Task17.kt
+++ src/main/kotlin/y2024/Task17.kt
@@ -13,24 +13,24 @@ class Task17(val input: List<String>) : Task {
 //		Register C: 0
 //
 //		Program: 0,1,5,4,3,0
-		var regA = input[0].split(": ")[1].toInt()
-		var regB = input[1].split(": ")[1].toInt()
-		var regC = input[2].split(": ")[1].toInt()
+		var regA = input[0].split(": ")[1].toLong()
+		var regB = input[1].split(": ")[1].toLong()
+		var regC = input[2].split(": ")[1].toLong()
 		val program = input[4].replace("Program:", "").split(",").map { it.trim().toInt() }
 //		println(regA)
 //		println(regB)
 //		println(regC)
 //		println(program)
-		val output = mutableListOf<Int>()
+		val output = mutableListOf<Long>()
 //		for (j in program.indices step 2) {
 		var index = 0
 
 		while (index < program.size) {
 			val i = program[index]
-			val ii = program[index + 1]
+			val ii = program[index + 1].toLong()
 			when (i) {
 				0 -> {
-					regA = adv(ii, regA, regB, regC).toInt()
+					regA = adv(ii, regA, regB, regC).toInt().toLong()
 				}
 				1 -> {
 					regB = bxl(ii, regA, regB, regC)
@@ -39,24 +39,23 @@ class Task17(val input: List<String>) : Task {
 					regB = bst(ii, regA, regB, regC)
 				}
 				3 -> {
-					if(regA != 0) {
-						val jump = jnz(ii, regA, regB, regC)
+					if(regA != 0L) {
+						val jump = jnz(ii, regA, regB, regC).toInt()
 						index = jump
 						continue
 					}
 				}
-
 				4 -> {
-					regC = bxc(ii, regA, regB, regC)
+					regB = bxc(ii, regA, regB, regC)
 				}
 				5 -> {
 					output.add(out(ii, regA, regB, regC))
 				}
 				6 -> {
-					regB = bdv(ii, regA, regB, regC).toInt()
+					regB = bdv(ii, regA, regB, regC).toInt().toLong()
 				}
 				7 -> {
-					regC = cdv(ii, regA, regB, regC).toInt()
+					regC = cdv(ii, regA, regB, regC).toInt().toLong()
 				}
 				else -> 0
 			}
@@ -66,65 +65,39 @@ class Task17(val input: List<String>) : Task {
 		return output.joinToString(",")
 	}
 
-	private fun cdv(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		// The cdv instruction (opcode 7) works exactly like the adv instruction
-		// except that the result is stored in the C register. (The numerator is still read from the A register.)
-		return adv(i,regA, regB, regC)
-	}
-
-	private fun bdv(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		//	The bdv instruction (opcode 6) works exactly like the adv instruction except that the result
-		//	is stored in the B register. (The numerator is still read from the A register.)
-		return adv(i,regA, regB, regC)
-	}
-
-	private fun out(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		//	The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
-		//	(If a program outputs multiple values, they are separated by commas.)
-		return expandOp(i,regA, regB, regC) % 8
-	}
-
-	private fun bxc(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		// The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result
-		// in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
-		return regB xor regC
-	}
-
-	private fun jnz(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		//The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it
-		//jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the
-		//instruction pointer is not increased by 2 after this instruction.
-		return i
-	}
-
-	private fun bst(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		//	The bst instruction (opcode 2) calculates the value of its combo operand modulo 8
-		//	(thereby keeping only its lowest 3 bits), then writes that value to the B register.
-		return expandOp(i,regA, regB, regC) % 8
-	}
-
-	private fun bxl(i: Int, regA: Int, regB: Int, regC: Int): Int {
-		// The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the
-		// instruction's literal operand, then stores the result in register B.
-		return regB xor i
-	}
-
-	private fun adv(operand: Int, a: Int, b: Int, c: Int): Int {
-		//	The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is
-//found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an
-//operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written
-//to the A register.
-		val op = expandOp(operand, a, b, c)
-		val denominator = 2.0.pow(op.toDouble()).toInt()
-		val result = a / denominator
+	private fun adv(operand: Long, regA: Long, regB:Long, regC: Long): Long {
+		val op = expandOp(operand, regA, regB, regC)
+		val denominator = 2.0.pow(op.toDouble()).toLong()
+		val result = regA / denominator
 		return result
 	}
+	private fun bxl(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return regB xor i
+	}
+	private fun bst(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return expandOp(i,regA, regB, regC) % 8
+	}
+	private fun jnz(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return i
+	}
+	private fun bxc(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return regB xor regC
+	}
+	private fun out(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return expandOp(i,regA, regB, regC) % 8
+	}
+	private fun bdv(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return adv(i,regA, regB, regC)
+	}
+	private fun cdv(i: Long, regA: Long, regB:Long, regC: Long): Long {
+		return adv(i,regA, regB, regC)
+	}
 
-	private fun expandOp(operand: Int, a: Int, b: Int, c: Int): Int {
+	private fun expandOp(operand: Long, regA: Long, regB:Long, regC: Long): Long {
 		return when (operand) {
-			4 -> a
-			5 -> b
-			6 -> c
+			4L -> regA
+			5L -> regB
+			6L -> regC
 			else -> operand
 		}
 	}
--- src/test/kotlin/y2024/Task17Test.kt
+++ src/test/kotlin/y2024/Task17Test.kt
@@ -14,10 +14,18 @@ class Task17Test {
 		
 		Program: 0,1,5,4,3,0
 	""".trimIndent().lines()
+	val test_input2 = """
+		Register A: 47719761
+		Register B: 0
+		Register C: 0
+		
+		Program: 2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0
+	""".trimIndent().lines()
 
 	@Test
 	fun a() {
 		assertEquals("4,6,3,5,6,3,5,2,1,0", Task17(test_input1).a())
+		assertEquals("7,0,3,1,2,6,3,7,1", Task17(test_input2).a())
 		val input = readInputAsListOfStrings("~/git/aoc-inputs/2024/2024_17.txt")
 		assertEquals(0, Task17(input).a())
 	}
