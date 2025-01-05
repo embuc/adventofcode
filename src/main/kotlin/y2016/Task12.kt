fd167f28fb448e42319b0edc109d343c962b6b1e
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-01-05 20:01:53 +0100

    Optimized for speed.

--- src/main/kotlin/y2016/Task12.kt
+++ src/main/kotlin/y2016/Task12.kt
@@ -5,52 +5,81 @@ import Task
 //--- Day 12: Leonardo's Monorail ---
 class Task12(val input: List<String>) : Task {
 
-	private data class Instruction(val command: String, val arg1: String, val arg2: String)
+	private data class Instruction(val command: String, val isArg1Int:Boolean, val arg1asInt: Int, val arg1asIx:Int,
+								   val isArg2Int:Boolean, val arg2asInt: Int, val arg2asIx:Int)
 
 	override fun a(): Any {
-		val registers = mutableMapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)
+		//"a" to 0, "b" to 0, "c" to 1, "d" to 0)
+		val registers = IntArray(4)
 		val instructions = toInstructions()
 		return processInstructions(instructions, registers)
 	}
 
 	private fun processInstructions(
 		instructions: MutableList<Instruction>,
-		registers: MutableMap<String, Int>
+		registers: IntArray
 	): Int {
 		var ix = 0
 		while (ix in instructions.indices) {
 			val instruction = instructions[ix]
 			when (instruction.command) {
 				"cpy" -> {
-					val value = if (instruction.arg1.toIntOrNull() == null) registers[instruction.arg1]!! else instruction.arg1.toInt()
-					registers[instruction.arg2] = value
+					val value = if (instruction.isArg1Int) instruction.arg1asInt else registers[instruction.arg1asIx]
+					registers[instruction.arg2asIx] = value
 				}
 
-				"inc" -> registers[instruction.arg1] = registers[instruction.arg1]!! + 1
-				"dec" -> registers[instruction.arg1] = registers[instruction.arg1]!! - 1
+				"inc" -> registers[instruction.arg1asIx] = registers[instruction.arg1asIx] + 1
+				"dec" -> registers[instruction.arg1asIx] = registers[instruction.arg1asIx] - 1
 				"jnz" -> {
-					val value = if (instruction.arg1.toIntOrNull() == null) registers[instruction.arg1]!! else instruction.arg1.toInt()
+					val value = if (instruction.isArg1Int) instruction.arg1asInt else registers[instruction.arg1asIx]
 					if (value != 0) {
-						ix += instruction.arg2.toInt() - 1
+						ix += instruction.arg2asInt - 1
 					}
 				}
 			}
 			ix++
 		}
-		return registers["a"]!!
+		return registers[0]
 	}
 
 	private fun toInstructions(): MutableList<Instruction> {
 		val instructions = mutableListOf<Instruction>()
 		for (i in input) {
 			val parts = i.split(" ")
-			instructions.add(Instruction(parts[0], parts[1], parts.getOrNull(2) ?: ""))
+			val command = parts[0]
+			val isArg1Int = parts[1].toIntOrNull() != null
+			val arg1asInt = parts[1].toIntOrNull() ?: 0
+			var arg1asIx = -1
+			if(!isArg1Int) {
+				arg1asIx = when (parts[1]) {
+					"a" -> 0
+					"b" -> 1
+					"c" -> 2
+					"d" -> 3
+					else -> -1
+				}
+			}
+			val isArg2Int = parts.getOrNull(2)?.toIntOrNull() != null
+			val arg2asInt = parts.getOrNull(2)?.toIntOrNull() ?: 0
+			var arg2asIx = -1
+			if(!isArg2Int && parts.getOrNull(2) != null) {
+				arg2asIx = when (parts[2]) {
+					"a" -> 0
+					"b" -> 1
+					"c" -> 2
+					"d" -> 3
+					else -> -1
+				}
+			}
+			instructions.add(Instruction(command, isArg1Int, arg1asInt, arg1asIx, isArg2Int, arg2asInt, arg2asIx))
 		}
 		return instructions
 	}
 
 	override fun b(): Int {
-		val registers = mutableMapOf("a" to 0, "b" to 0, "c" to 1, "d" to 0)
+		//"a" to 0, "b" to 0, "c" to 1, "d" to 0)
+		val registers = IntArray(4)
+		registers[2] = 1 //c = 1
 		val instructions = toInstructions()
 		return processInstructions(instructions, registers)
 	}
