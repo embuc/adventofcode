a31831f737de8a3144164a54b6f2ee9e9ea4ab69
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-01-15 22:01:21 +0100

    Task 10 part 2.

--- src/main/kotlin/y2017/Task10.kt
+++ src/main/kotlin/y2017/Task10.kt
@@ -3,19 +3,20 @@ package y2017
 import Task
 
 //--- Day 10: Knot Hash ---
-class Task10(val input:List<Int>, private val limit:Int):Task {
+class Task10(val stringInput: String, private val limit: Int) : Task {
 
 	override fun a(): Any {
-		val array = (0..limit).mapIndexed {index,_ ->  index}.toIntArray()
+		val input = stringInput.split(",").map { it.toInt() }
+		val array = (0..limit).mapIndexed { index, _ -> index }.toIntArray()
 		var currentPosition = 0
 		var skipSize = 0
 		for (length in input) {
-			val sublist = IntArray(length) {0}
-			for (i in 0..<length){
+			val sublist = IntArray(length) { 0 }
+			for (i in 0..<length) {
 				val currIx = (currentPosition + i) % array.size
 				sublist[length - 1 - i] = array[currIx]
 			}
-			for (i in 0..<length){
+			for (i in 0..<length) {
 				val currIx = (currentPosition + i) % array.size
 				array[currIx] = sublist[i]
 			}
@@ -27,6 +28,35 @@ class Task10(val input:List<Int>, private val limit:Int):Task {
 	}
 
 	override fun b(): Any {
-		return -1
+		val input = stringInput.map { it.code } + listOf(17, 31, 73, 47, 23)
+		val array = (0..limit).mapIndexed { index, _ -> index }.toIntArray()
+		var currentPosition = 0
+		var skipSize = 0
+		repeat(64) {
+			for (length in input) {
+				val sublist = IntArray(length) { 0 }
+				for (i in 0..<length) {
+					val currIx = (currentPosition + i) % array.size
+					sublist[length - 1 - i] = array[currIx]
+				}
+				for (i in 0..<length) {
+					val currIx = (currentPosition + i) % array.size
+					array[currIx] = sublist[i]
+				}
+				currentPosition += length + skipSize
+				currentPosition %= array.size
+				skipSize++
+			}
+		}
+
+		//find dense hash 16 * 16 numbers
+		val denseHash = IntArray(16) { 0 }
+		for (i in 0 until 16) {
+			val start = i * 16
+			val end = start + 16
+			val sublist = array.slice(start until end)
+			denseHash[i] = sublist.reduce { acc, i -> acc xor i }
+		}
+		return denseHash.map { it.toString(16).padStart(2, '0') }.joinToString("")
 	}
 }
--- src/test/kotlin/y2017/Task10Test.kt
+++ src/test/kotlin/y2017/Task10Test.kt
@@ -8,12 +8,14 @@ class Task10Test {
 
 	@Test
 	fun a() {
-		assertEquals(12, Task10(listOf(3, 4, 1, 5), 4).a())
 		val input = readInputAsString("~/git/aoc-inputs/2017/2017_10.txt")
-		assertEquals(3770, Task10(input.split(",").map { it.toInt() }, 255).a())
+		assertEquals(4480, Task10(input, 255).a())
 	}
 
 	@Test
 	fun b() {
+        assertEquals("33efeb34ea91902bb2f59c9920caa6cd", Task10("AoC 2017", 255).b())
+        val input = readInputAsString("~/git/aoc-inputs/2017/2017_10.txt")
+        assertEquals("c500ffe015c83b60fad2e4b7d59dabc4", Task10(input, 255).b())
 	}
 }
