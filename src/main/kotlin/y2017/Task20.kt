eb32f9aa39b54330d8686222bad5f848e6fe4b8d
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-01-19 16:49:45 +0100

    Task 20 part 2.

--- src/main/kotlin/y2017/Task20.kt
+++ src/main/kotlin/y2017/Task20.kt
@@ -12,14 +12,7 @@ class Task20(val input:List<String>) : Task {
 	)
 
 	override fun a(): Any {
-		val particles = mutableListOf<Particle>()
-		for (l in input) {
-			val parts = l.split(", ").map { it.split("=") }
-			val p = parts[0][1].split(",").map { it.replace("<","").replace(">", "").toLong() }
-			val v = parts[1][1].split(",").map { it.replace("<","").replace(">", "").toLong() }
-			val a = parts[2][1].split(",").map { it.replace("<","").replace(">", "").toLong() }
-			particles.add(Particle(Triple(p[0], p[1], p[2]), Triple(v[0], v[1], v[2]), Triple(a[0], a[1], a[2])))
-		}
+		val particles = parseParticles()
 		repeat(400) {
 			for (p in particles) {
 				p.v = Triple(p.v.first + p.a.first, p.v.second + p.a.second, p.v.third + p.a.third)
@@ -30,6 +23,28 @@ class Task20(val input:List<String>) : Task {
 	}
 
 	override fun b(): Any {
-		return 0
+		val particles = parseParticles()
+		repeat(400) {
+			for (p in particles) {
+				p.v = Triple(p.v.first + p.a.first, p.v.second + p.a.second, p.v.third + p.a.third)
+				p.p = Triple(p.p.first + p.v.first, p.p.second + p.v.second, p.p.third + p.v.third)
+			}
+			val positions = particles.map { it.p }
+			val duplicates = positions.groupingBy { it }.eachCount().filter { it.value > 1 }.keys
+			particles.removeIf { it.p in duplicates }
+		}
+		return particles.size
+	}
+
+	private fun parseParticles(): MutableList<Particle> {
+		val particles = mutableListOf<Particle>()
+		for (l in input) {
+			val parts = l.split(", ").map { it.split("=") }
+			val p = parts[0][1].split(",").map { it.replace("<", "").replace(">", "").toLong() }
+			val v = parts[1][1].split(",").map { it.replace("<", "").replace(">", "").toLong() }
+			val a = parts[2][1].split(",").map { it.replace("<", "").replace(">", "").toLong() }
+			particles.add(Particle(Triple(p[0], p[1], p[2]), Triple(v[0], v[1], v[2]), Triple(a[0], a[1], a[2])))
+		}
+		return particles
 	}
 }
