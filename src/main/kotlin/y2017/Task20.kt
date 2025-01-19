eeccfb06fe5322c7b3138c2513c20ffe30bf68dd
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-01-19 16:45:13 +0100

    Task 20 part 1.

---
+++ src/main/kotlin/y2017/Task20.kt
@@ -0,0 +1,35 @@
+package y2017
+
+import Task
+import kotlin.math.abs
+
+//--- Day 20: Particle Swarm ---
+class Task20(val input:List<String>) : Task {
+	private data class Particle(
+		var p: Triple<Long, Long, Long>,
+		var v: Triple<Long, Long, Long>,
+		var a: Triple<Long, Long, Long>
+	)
+
+	override fun a(): Any {
+		val particles = mutableListOf<Particle>()
+		for (l in input) {
+			val parts = l.split(", ").map { it.split("=") }
+			val p = parts[0][1].split(",").map { it.replace("<","").replace(">", "").toLong() }
+			val v = parts[1][1].split(",").map { it.replace("<","").replace(">", "").toLong() }
+			val a = parts[2][1].split(",").map { it.replace("<","").replace(">", "").toLong() }
+			particles.add(Particle(Triple(p[0], p[1], p[2]), Triple(v[0], v[1], v[2]), Triple(a[0], a[1], a[2])))
+		}
+		repeat(400) {
+			for (p in particles) {
+				p.v = Triple(p.v.first + p.a.first, p.v.second + p.a.second, p.v.third + p.a.third)
+				p.p = Triple(p.p.first + p.v.first, p.p.second + p.v.second, p.p.third + p.v.third)
+			}
+		}
+		return particles.indexOf(particles.minBy { abs(it.p.first) + abs(it.p.second) + abs(it.p.third) })
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2017/Task20Test.kt
@@ -0,0 +1,20 @@
+package y2017
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import utils.readInputAsListOfStrings
+
+class Task20Test {
+
+	@Test
+	fun a() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2017/2017_20.txt")
+		assertEquals(119, Task20(input).a())
+	}
+
+	@Test
+	fun b() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2017/2017_20.txt")
+		assertEquals(119, Task20(input).b())
+	}
+}
