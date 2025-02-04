fac2346ee324210e5b9da7aae45167fcc989a2c7
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-04 19:36:44 +0100

    Task5 started.

---
+++ src/main/kotlin/y2022/Task5.kt
@@ -0,0 +1,18 @@
+package y2022
+
+import Task
+//--- Day 5: Supply Stacks ---
+class Task5(val input: List<String>):Task {
+
+
+	override fun a(): Any {
+		for (i in input) {
+			println(i)
+		}
+		return ""
+	}
+
+	override fun b(): Any {
+		return ""
+	}
+}
