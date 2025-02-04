7ac5622c9c9378b9f1772426853cc34d3f2b9918
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-04 12:29:11 +0100

    Cleanup.

--- src/main/kotlin/y2022/Task3.kt
+++ src/main/kotlin/y2022/Task3.kt
@@ -7,12 +7,9 @@ class Task3(val input: List<String>) : Task {
 	override fun a(): Any {
 		var sum = 0
 		for (line in input) {
-//			println(line)
 			val s1 = line.substring(0, line.length / 2).toList()
 			val s2 = line.substring(line.length / 2 ).toList()
 			val common = s1.intersect(s2)
-//			println("line length: ${line.length} s1 length: ${s1.size} s2 length: ${s2.size} common length: ${common.size}")
-//			println(common)
 			sum += getPriority(common)
 		}
 		return sum
@@ -36,11 +33,7 @@ class Task3(val input: List<String>) : Task {
 			val line1 = input[index].toSet()
 			val line2 = input[index + 1].toSet()
 			val line3 = input[index + 2].toSet()
-//			println(line)
-
 			val common = line1.intersect(line2).intersect(line3)
-//			println("line length: ${line.length} s1 length: ${s1.size} s2 length: ${s2.size} common length: ${common.size}")
-//			println(common)
 			sum += getPriority(common)
 		}
 		return sum
