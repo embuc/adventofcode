3cfbab8fc1cbc9706ea2e2408d16299f866a0acb
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-04 12:35:19 +0100

    Task 4 scaffold.

---
+++ src/main/kotlin/y2022/Task4.kt
@@ -0,0 +1,14 @@
+package y2022
+
+import Task
+
+//--- Day 4: Camp Cleanup ---
+class Task4(val input: List<String>) : Task {
+	override fun a(): Int {
+		return 0
+	}
+
+	override fun b(): Int {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2022/Task4Test.kt
@@ -0,0 +1,17 @@
+package y2022
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Task4Test {
+	@Test
+	fun a() {
+		assertEquals(0, Task4(listOf()).a())
+	}
+
+	@Test
+	fun b() {
+		assertEquals(0, Task4(listOf()).b())
+	}
+
+}
