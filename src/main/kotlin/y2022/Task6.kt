38b7ca7a86164c0dd4930ea7f958f90af575e8d2
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-05 11:02:56 +0100

    Refactor + Cleanup.

--- src/main/kotlin/y2022/Task6.kt
+++ src/main/kotlin/y2022/Task6.kt
@@ -5,31 +5,17 @@ import Task
 //--- Day 6: Tuning Trouble ---
 class Task6(val input: String) : Task {
 	override fun a(): Any {
-		val input = input.toCharArray()
-		for (i in input.indices) {
-			if (i < 3) {
-				continue
-			}
-			if (input[i] != input[i - 1] && input[i] != input[i - 2] && input[i] != input[i - 3] &&
-				input[i - 1] != input[i - 2] && input[i - 1] != input[i - 3] &&
-				input[i - 2] != input[i - 3]) {
-				return i + 1
-			}
-		}
-		return 1
+		return findUniqueSequence(4)
 	}
 
 	override fun b(): Any {
+		return findUniqueSequence(14)
+	}
+
+	private fun findUniqueSequence(length: Int): Int {
 		val input = input.toCharArray()
-		for (i in input.indices) {
-			if (i < 14) {
-				continue
-			}
-			val set = mutableSetOf<Char>()
-			for (j in 0..<14) {
-				set.add(input[i - j])
-			}
-			if (set.size == 14) {
+		for (i in length - 1 until input.size) {
+			if (input.slice(i - length + 1..i).toSet().size == length) {
 				return i + 1
 			}
 		}
