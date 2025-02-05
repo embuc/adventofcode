aefdf09653ba647c5544b524a98fda820c521673
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-05 10:58:10 +0100

    Task6 part 1.

--- src/main/kotlin/y2022/Task6.kt
+++ src/main/kotlin/y2022/Task6.kt
@@ -5,6 +5,17 @@ import Task
 //--- Day 6: Tuning Trouble ---
 class Task6(val input: String) : Task {
 	override fun a(): Any {
+		val input = input.toCharArray()
+		for (i in input.indices) {
+			if (i < 3) {
+				continue
+			}
+			if (input[i] != input[i - 1] && input[i] != input[i - 2] && input[i] != input[i - 3] &&
+				input[i - 1] != input[i - 2] && input[i - 1] != input[i - 3] &&
+				input[i - 2] != input[i - 3]) {
+				return i + 1
+			}
+		}
 		return 1
 	}
 
