24fce156957d606600457cc4ed51577a61207752
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-06 14:00:58 +0100

    Task 8 started.

---
+++ src/main/kotlin/y2022/Task8.kt
@@ -0,0 +1,14 @@
+package y2022
+
+import Task
+
+//--- Day 8: Treetop Tree House ---
+class Task8(val input: List<String>) : Task {
+	override fun a(): Any {
+		return 0
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2022/Task8Test.kt
@@ -0,0 +1,18 @@
+package y2022
+
+import org.junit.jupiter.api.Test
+import utils.readInputAsListOfStrings
+import kotlin.test.assertEquals
+
+class Task8Test {
+	@Test
+	fun a() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_8.txt")
+		assertEquals(123, Task8(input).a())
+	}
+
+	@Test
+	fun b() {
+	}
+
+}
