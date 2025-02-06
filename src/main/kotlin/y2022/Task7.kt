7731ea6275a8ea6d9d145af79dfe81eeee87e0d2
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-06 13:17:22 +0100

    Task 7 part 2.

--- src/main/kotlin/y2022/Task7.kt
+++ src/main/kotlin/y2022/Task7.kt
@@ -72,7 +72,59 @@ class Task7(val input: List<String>) : Task {
 		return matches
 	}
 
+	fun findDirectoriesOverThreshold(root: TreeNode<File>, threshold: Int): List<TreeNode<File>> {
+		val matches = mutableListOf<TreeNode<File>>()
+
+		root.traverseAndApply { node ->
+			// Only consider directories (nodes that have children or empty directories)
+			// and check their precalculated size
+			if (node.value.size >= threshold) {
+				matches.add(node)
+			}
+		}
+
+		return matches
+	}
+
 	override fun b(): Any {
-		return 1
+		val totalSize = 70000000
+		val unusedMinSize = 30000000
+		var i = 0
+		var node = TreeNode(File("/", mutableListOf(), 0), mutableListOf(), null)
+		while (i in input.indices) {
+			println(input[i])
+			val line = input[i]
+			val parts = line.split(" ")
+			if (parts[1] == "cd") {
+				if (parts[2] == "..") {
+					node = node.parent!!
+				} else if (parts[2] == "/") {
+					node = node.getRoot()
+				} else {
+					val dir = node.children.find { it.value.name == parts[2] }!!
+					node = dir
+				}
+			} else if (parts[0] == "dir") {
+				val dir = File(parts[1], mutableListOf(), 0)
+				node.addChild(dir)
+			} else if(parts[0].toIntOrNull() != null) {
+				val file = File(parts[1], mutableListOf(), parts[0].toInt())
+				node.value.children.add(file)
+			}
+			// if "ls" do nothing, just ignore e.g. skip to next line
+			i++
+		}
+		node.getRoot().printTree()
+		val root = node.getRoot()
+		calculateDirectorySizes(root)  // calculate all sizes (whole tree)
+
+		val threshold = node.getRoot().value.size - (totalSize - unusedMinSize)
+		val smallDirs = findDirectoriesOverThreshold(root, threshold)
+		node.getRoot().printTree()
+		println("Total size: ${node.getRoot().value.size}")
+		println("Unused size: $unusedMinSize")
+		println("Threshold: $threshold")
+
+		return smallDirs.minOf { it.value.size }
 	}
 }
--- src/test/kotlin/y2022/Task7Test.kt
+++ src/test/kotlin/y2022/Task7Test.kt
@@ -5,41 +5,17 @@ import org.junit.jupiter.api.Test
 import utils.readInputAsListOfStrings
 
 class Task7Test {
-	val testInput = """
-		${'$'} cd /
-		${'$'} ls
-		dir a
-		14848514 b.txt
-		8504156 c.dat
-		dir d
-		${'$'} cd a
-		${'$'} ls
-		dir e
-		29116 f
-		2557 g
-		62596 h.lst
-		${'$'} cd e
-		${'$'} ls
-		584 i
-		${'$'} cd ..
-		${'$'} cd ..
-		${'$'} cd d
-		${'$'} ls
-		4060174 j
-		8033020 d.log
-		5626152 d.ext
-		7214296 k
-""".trim().trimIndent().lines().map { it.trim() }
 
 	@Test
 	fun a() {
-		assertEquals(95437, Task7(testInput).a())
 		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_7.txt")
-		assertEquals(1, Task7(input).a())
+		assertEquals(1783610, Task7(input).a())
 	}
 
 	@Test
 	fun b() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_7.txt")
+		assertEquals(4370655, Task7(input).b())
 	}
 
 }
