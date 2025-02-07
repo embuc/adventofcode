5c903b698330d0d6b5a5e199085b7da327e6b452
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-07 17:15:53 +0100

    Task 10 started.

---
+++ src/main/kotlin/y2022/Task10.kt
@@ -0,0 +1,15 @@
+package y2022
+
+import Task
+
+//--- Day 10: Cathode-Ray Tube ---
+class Task10(val input: List<String>) : Task {
+
+	override fun a(): Any {
+		return 0
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
