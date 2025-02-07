2c883d4052231cd362c97b7662de17c77c32aedc
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-07 16:44:19 +0100

    Task 9 part1.

--- src/main/kotlin/y2022/Task9.kt
+++ src/main/kotlin/y2022/Task9.kt
@@ -1,11 +1,69 @@
 package y2022
 
 import Task
+import kotlin.math.abs
 
 //--- Day 9: Rope Bridge ---
 class Task9(val input: List<String>) : Task {
+
 	override fun a(): Any {
-		return 0
+		var head: Pair<Int, Int> = Pair(0, 0)
+		var tail: Pair<Int, Int> = Pair(0, 0)
+		val visited = mutableSetOf<Pair<Int, Int>>()
+		visited.add(tail)
+		for (line in input) {
+			val (direction, steps) = line.split(" ")
+			println("dir: $direction steps: $steps")
+			repeat(steps.toInt()) {
+				when (direction) {
+					"L" -> {
+						head = Pair(head.first, head.second - 1)
+						tail = calculateTail(head, tail)
+					}
+
+					"D" -> {
+						head = Pair(head.first + 1, head.second)
+						tail = calculateTail(head, tail)
+					}
+
+					"R" -> {
+						head = Pair(head.first, head.second + 1)
+						tail = calculateTail(head, tail)
+					}
+
+					"U" -> {
+						head = Pair(head.first - 1, head.second)
+						tail = calculateTail(head, tail)
+					}
+				}
+				visited.add(tail)
+			}
+
+		}
+		return visited.size
+	}
+
+	private fun calculateTail(
+		head: Pair<Int, Int>,
+		tail: Pair<Int, Int>
+	): Pair<Int, Int> {
+		// if head within all neighbors of tail or on the same tile, return same tail
+		if (abs(head.first - tail.first) <= 1 && abs(head.second - tail.second) <= 1
+		) {
+			return tail
+		}
+		// else check if head is on the same row or column as tail and return the new tail updated by that row or column
+		if (head.first == tail.first) {
+			return Pair(tail.first, tail.second + if (head.second > tail.second) 1 else -1)
+		}
+		if (head.second == tail.second) {
+			return Pair(tail.first + if (head.first > tail.first) 1 else -1, tail.second)
+		}
+		// else return the new tail updated by the diagonal
+		return Pair(
+			tail.first + if (head.first > tail.first) 1 else -1,
+			tail.second + if (head.second > tail.second) 1 else -1
+		)
 	}
 
 	override fun b(): Any {
--- src/test/kotlin/y2022/Task9Test.kt
+++ src/test/kotlin/y2022/Task9Test.kt
@@ -1,12 +1,26 @@
 package y2022
 
 import org.junit.jupiter.api.Test
+import utils.readInputAsListOfStrings
 import kotlin.test.assertEquals
 
 class Task9Test {
+	val testInput = """
+		R 4
+		U 4
+		L 3
+		D 1
+		R 4
+		D 1
+		L 5
+		R 2
+		""".trim().split("\n").map { it.trim() }.toList()
+
 	@Test
 	fun a() {
-		assertEquals(0, Task9(listOf()).a())
+		assertEquals(13, Task9(testInput).a())
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_9.txt")
+		assertEquals(0, Task9(input).a())
 	}
 
 	@Test
