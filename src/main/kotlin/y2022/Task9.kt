cbcbada1d9b1f4527ab635aab2a6573275acdfc1
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-07 08:52:11 +0100

    Task 9 started.

---
+++ src/main/kotlin/y2022/Task9.kt
@@ -0,0 +1,14 @@
+package y2022
+
+import Task
+
+//--- Day 9: Rope Bridge ---
+class Task9(val input: List<String>) : Task {
+	override fun a(): Any {
+		return 0
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2022/Task9Test.kt
@@ -0,0 +1,16 @@
+package y2022
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Task9Test {
+	@Test
+	fun a() {
+		assertEquals(0, Task9(listOf()).a())
+	}
+
+	@Test
+	fun b() {
+	}
+
+}
