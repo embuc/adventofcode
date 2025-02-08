97c8d86bfc0b6c2af57a0af85539a8ff3a8948e6
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-08 14:18:46 +0100

    Started Task 12.

---
+++ src/main/kotlin/y2022/Task12.kt
@@ -0,0 +1,15 @@
+package y2022
+
+import Task
+
+//--- Day 12: Hill Climbing Algorithm ---
+class Task12(val input: List<String>) : Task {
+
+	override fun a(): Any {
+		return 0
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2022/Task12Test.kt
@@ -0,0 +1,18 @@
+package y2022
+
+import org.junit.jupiter.api.Test
+import utils.readInputAsListOfStrings
+import kotlin.test.assertEquals
+
+class Task12Test {
+	@Test
+	fun a() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_12.txt")
+		 assertEquals(0, Task12(input).a())
+	}
+
+	@Test
+	fun b() {
+	}
+
+}
