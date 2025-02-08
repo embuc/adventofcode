fec25b9b34c34fbab21981897dd02919bdfcc2d6
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-08 14:13:53 +0100

    Task 11 part 2.

--- src/main/kotlin/y2022/Task11.kt
+++ src/main/kotlin/y2022/Task11.kt
@@ -1,54 +1,44 @@
 package y2022
 
 import Task
+import utils.gcd
 
 //--- Day 11: Monkey in the Middle ---
 class Task11(val input: List<String>) : Task {
 
 	private data class Monkey(
 		val name: String,
-		val items: MutableList<Int>,
-		val testDivisible: Int,
-		var operation: (Int) -> Int,
+		val items: MutableList<Long>,
+		val testDivisible: Long,
+		var operation: (Long) -> Long,
 		val ifTrue: String,
 		val ifFalse: String,
-		var inspected:Int = 0
+		var inspected: Long = 0
 	)
 
 	override fun a(): Any {
-		val monkeys = mutableListOf<Monkey>()
-		for ((ix, line) in input.withIndex()) {
-			if (line.isEmpty()) {
-				continue
-			}
-			if (line.startsWith("Monkey")) {
-				val name = line.split(" ")[1].replace(":", "")
-				val items = mutableListOf<Int>()
-				input[ix + 1].split(" ").map { it.replace(",", "") }.filter { it.toIntOrNull() != null }.forEach { items.add(it.toInt()) }
-				val testDivisible = input[ix + 3].trim().split(" ")[3].toInt()
-				val op = input[ix + 2].trim().split(" ")
-				val b = op[5].toIntOrNull()
-				val operand = op[4]
-				val operation = when (operand) {
-					"*" -> { x: Int -> x * (b ?: x) }
-					"+" -> { x: Int -> x + (b ?: x) }
-					else -> { x: Int -> x }
-				}
-				val ifTrue = input[ix + 4].trim().split(" ")[5]
-				val ifFalse = input[ix + 5].trim().split(" ")[5]
-				val monkey = Monkey(name, items, testDivisible, operation, ifTrue, ifFalse)
-				monkeys.add(monkey)
-			}
-		}
-		//map monkeys to their names
+		return monkeyBusiness(20, divideByThree = true);
+	}
+
+	override fun b(): Any {
+		return monkeyBusiness(10_000, divideByThree = false);
+	}
+
+	private fun monkeyBusiness(rounds: Int, divideByThree: Boolean): Long {
+		val monkeys = parseMonkeys()
 		val monkeyMap = monkeys.associateBy { it.name }
-		repeat(20) {
+		val superModulo = getLCM(monkeys.map { it.testDivisible })
+		repeat(rounds) { iteration ->
 			for (monkey in monkeys) {
 				for (item in monkey.items) {
 					monkey.inspected++
 					var newItem = monkey.operation(item)
-					newItem = newItem / 3
-					if (newItem % monkey.testDivisible == 0) {
+					if (divideByThree){
+						newItem = newItem / 3
+					} else {
+						newItem = newItem % superModulo
+					}
+					if (newItem % monkey.testDivisible == 0L) {
 						monkeyMap[monkey.ifTrue]!!.items.add(newItem)
 					} else {
 						monkeyMap[monkey.ifFalse]!!.items.add(newItem)
@@ -56,14 +46,51 @@ class Task11(val input: List<String>) : Task {
 				}
 				monkey.items.clear()
 			}
+			if((iteration+1) % 1000 == 0 || (iteration+1) == 1 || (iteration+1) == 20){
+				println("Round ${iteration+1}")
+				for(monkey in monkeys){
+					println("${monkey.name} ${monkey.inspected}")
+				}
+				println()
+			}
 		}
 		monkeys.sortByDescending { it.inspected }
-//		monkeys.forEach { println(it) }
-
 		return monkeys[0].inspected * monkeys[1].inspected
 	}
 
-	override fun b(): Any {
-		return 0
+	private fun getLCM(longs: List<Long>): Long {
+		var lcm = longs[0]
+		for (i in 1 until longs.size) {
+			lcm = lcm * longs[i] / gcd(lcm, longs[i])
+		}
+		return lcm
+	}
+
+	private fun parseMonkeys(): MutableList<Monkey> {
+		val monkeys = mutableListOf<Monkey>()
+		for ((ix, line) in input.withIndex()) {
+			if (line.isEmpty()) {
+				continue
+			}
+			if (line.startsWith("Monkey")) {
+				val name = line.split(" ")[1].replace(":", "")
+				val items = mutableListOf<Long>()
+				input[ix + 1].split(" ").map { it.replace(",", "") }.filter { it.toIntOrNull() != null }.forEach { items.add(it.toLong()) }
+				val testDivisible = input[ix + 3].trim().split(" ")[3].toLong()
+				val op = input[ix + 2].trim().split(" ")
+				val b = op[5].toLongOrNull()
+				val operand = op[4]
+				val operation = when (operand) {
+					"*" -> { x: Long -> x * (b ?: x) }
+					"+" -> { x: Long -> x + (b ?: x) }
+					else -> { x: Long -> x }
+				}
+				val ifTrue = input[ix + 4].trim().split(" ")[5]
+				val ifFalse = input[ix + 5].trim().split(" ")[5]
+				val monkey = Monkey(name, items, testDivisible, operation, ifTrue, ifFalse)
+				monkeys.add(monkey)
+			}
+		}
+		return monkeys
 	}
 }
--- src/test/kotlin/y2022/Task11Test.kt
+++ src/test/kotlin/y2022/Task11Test.kt
@@ -6,14 +6,46 @@ import kotlin.test.assertEquals
 
 class Task11Test {
 
+	val testInput = """
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+	""".split("\n")
+
 	@Test
 	fun a() {
 		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_11.txt")
-		assertEquals(50616, Task11(input).a())
+		assertEquals(50616L, Task11(input).a())
 	}
 
 	@Test
 	fun b() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_11.txt")
+		assertEquals(11309046332L, Task11(input).b())
 	}
 
 }
