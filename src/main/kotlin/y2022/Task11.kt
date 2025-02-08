a33961774c2f3fe664b664a2d8de7b0017b2d248
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-08 10:56:34 +0100

    Started Task 11.

---
+++ src/main/kotlin/y2022/Task11.kt
@@ -0,0 +1,14 @@
+package y2022
+
+import Task
+//--- Day 11: Monkey in the Middle ---
+class Task11(val input :List<String>) : Task {
+
+	override fun a(): Any {
+		return 0
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2022/Task11Test.kt
@@ -0,0 +1,19 @@
+package y2022
+
+import org.junit.jupiter.api.Test
+import utils.readInputAsListOfStrings
+import kotlin.test.assertEquals
+
+class Task11Test {
+
+	@Test
+	fun a() {
+		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_11.txt")
+		assertEquals(0, Task11(input).a())
+	}
+
+	@Test
+	fun b() {
+	}
+
+}
