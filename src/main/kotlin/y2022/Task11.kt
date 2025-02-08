72c034712375be632f6885688d917f325a61e564
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-08 12:10:40 +0100

    Task 11 part 1.

--- src/main/kotlin/y2022/Task11.kt
+++ src/main/kotlin/y2022/Task11.kt
@@ -1,11 +1,75 @@
 package y2022
 
 import Task
+
 //--- Day 11: Monkey in the Middle ---
-class Task11(val input :List<String>) : Task {
+class Task11(val input: List<String>) : Task {
+
+	private data class Monkey(
+		val name: String,
+		val items: MutableList<Int>,
+		val testDivisible: Int,
+		var operation: (Int) -> Int,
+		val ifTrue: String,
+		val ifFalse: String,
+		var inspected:Int = 0
+	)
 
 	override fun a(): Any {
-		return 0
+		val monkeys = mutableListOf<Monkey>()
+		for ((ix, line) in input.withIndex()) {
+			if (line.isEmpty()) {
+				continue
+			}
+			if (line.startsWith("Monkey")) {
+				println(line)
+				println(input[ix + 1])
+				println(input[ix + 2])
+				println(input[ix + 3])
+				println(input[ix + 4])
+				println(input[ix + 5])
+				val name = line.split(" ")[1].replace(":", "")
+				val items = mutableListOf<Int>()
+				input[ix + 1].split(" ").map { it.replace(",", "") }.filter { it.toIntOrNull() != null }.forEach { items.add(it.toInt()) }
+				val testDivisible = input[ix + 3].trim().split(" ")[3].toInt()
+				val op = input[ix + 2].trim().split(" ")
+				val b = op[5].toIntOrNull()
+				val operand = op[4]
+				val operation = when (operand) {
+					"*" -> { x: Int -> x * (b ?: x) }
+					"+" -> { x: Int -> x + (b ?: x) }
+					else -> { x: Int -> x }
+				}
+				val ifTrue = input[ix + 4].trim().split(" ")[5]
+				val ifFalse = input[ix + 5].trim().split(" ")[5]
+				val monkey = Monkey(name, items, testDivisible, operation, ifTrue, ifFalse)
+				monkeys.add(monkey)
+				println(monkey)
+			}
+		}
+		//map monkeys to their names
+		val monkeyMap = monkeys.associateBy { it.name }
+		repeat(20) {
+			for (monkey in monkeys) {
+				for (item in monkey.items) {
+					monkey.inspected++
+					println(item)
+					var newItem = monkey.operation(item)
+					newItem = newItem / 3
+					if (newItem % monkey.testDivisible == 0) {
+						monkeyMap[monkey.ifTrue]!!.items.add(newItem)
+					} else {
+						monkeyMap[monkey.ifFalse]!!.items.add(newItem)
+					}
+				}
+				monkey.items.clear()
+			}
+		}
+		monkeys.sortByDescending { it.inspected }
+		monkeys.forEach { println(it) }
+
+//		monkeys.forEach { println(it) }
+		return monkeys[0].inspected * monkeys[1].inspected
 	}
 
 	override fun b(): Any {
--- src/test/kotlin/y2022/Task11Test.kt
+++ src/test/kotlin/y2022/Task11Test.kt
@@ -9,7 +9,7 @@ class Task11Test {
 	@Test
 	fun a() {
 		val input = readInputAsListOfStrings("~/git/aoc-inputs/2022/2022_11.txt")
-		assertEquals(0, Task11(input).a())
+		assertEquals(50616, Task11(input).a())
 	}
 
 	@Test
