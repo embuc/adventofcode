4137383b558e6884602ad4a968890676afd7ce5b
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-02-09 19:04:11 +0100

    Task 14 started.

---
+++ src/main/kotlin/y2022/Task14.kt
@@ -0,0 +1,15 @@
+package y2022
+
+import Task
+
+//--- Day 14: Regolith Reservoir ---
+class Task14(val input: List<String>) : Task {
+
+	override fun a(): Any {
+		return 0
+	}
+
+	override fun b(): Any {
+		return 0
+	}
+}
---
+++ src/test/kotlin/y2022/Task14Test.kt
@@ -0,0 +1,16 @@
+package y2022
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Task14Test {
+	@Test
+	fun a() {
+		assertEquals(0, Task14(emptyList()).a())
+	}
+
+	@Test
+	fun b() {
+	}
+
+}
