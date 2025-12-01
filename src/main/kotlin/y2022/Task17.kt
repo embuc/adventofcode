35428235b2a119387332f8cb66dc3b187539e612
Emir Bucalovic <emir.bucalovic@tactel.se>
2025-12-01 11:08:03 +0100

    Cleanup.

--- src/main/kotlin/y2022/Task17.kt
+++ src/main/kotlin/y2022/Task17.kt
@@ -4,8 +4,29 @@ import Task
 
 //--- Day 17: Pyroclastic Flow ---
 class Task17(val input: String) : Task {
+
+//	####
+//
+//	.#.
+//	###
+//	.#.
+//
+//	..#
+//	..#
+//	###
+//
+//	#
+//	#
+//	#
+//	#
+//
+//	##
+//	##
+
+
 	override fun a(): Any {
-		
+		//how high tower after 2022 rocks falling
+
 		return 0
 	}
 
--- src/test/kotlin/y2022/Task17Test.kt
+++ src/test/kotlin/y2022/Task17Test.kt
@@ -1,14 +1,14 @@
 package y2022
 
 import org.junit.jupiter.api.Test
-import utils.readInputAsString
-import kotlin.test.assertEquals
 
 class Task17Test {
 	@Test
 	fun a() {
-		val input = readInputAsString("~/git/aoc-inputs/2022/2022_17.txt")
-		assertEquals(2124, Task17(input).a())
+		val testInput = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+//		assertEquals(3068, Task17(testInput).a()) TODO
+//		val input = readInputAsString("~/git/aoc-inputs/2022/2022_17.txt")
+//		assertEquals(2124, Task17(input).a())
 	}
 
 	@Test
